0d90a5a0c218a25e07df86fde641d7ef00199d39
M Gopalakrishnan <geekrootml@gmail.com>
2025-01-27 01:37:45 +0530

    Loging and Exception

--- setup.py
+++ setup.py
@@ -3,10 +3,18 @@ from typing import List
 
 
 HYPEN_E_DOT = '-e .'
+
 def get_requirements(file_path:str) -> List[str]:
     '''
         This function returns a list of requirements from a file
     '''
+    # requirements = []
+    # with open(file_path, 'r') as file_obj:
+    #     requirements = [req.replace('\n', '') for req in file_obj]  # Iterate directly over file_obj
+
+    #     if HYPEN_E_DOT in requirements:
+    #         requirements.remove(HYPEN_E_DOT)
+    # return requirements
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
